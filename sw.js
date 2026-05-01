180c2ed5649be870db91c350371471a682842376
tezike1979-ai <tezike1979@gmail.com>
2026-05-01 17:03:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "junior-vocab-v6";
+const CACHE_NAME = "junior-vocab-v7";
 const STATIC_ASSETS = ["./index.html", "./manifest.json"];
 
 self.addEventListener("install", event => {
